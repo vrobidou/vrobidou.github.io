307f6d83aaab7eba52dfeba2189378ab678425b4
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2015-07-13 15:15:05 +0200

    Add a header navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,13 @@
 {% include js/bootstrap/scrollspy.js %}
 {% include js/bootstrap/tab.js %}
 {% include js/bootstrap/affix.js %}
+$(document).ready(function() {
+	/* Scrollspy */
+	$('body').scrollspy({ target: '#navbar' });
+	/* Navbar link */
+	$('#navbar a').click(function() {
+		var id = $(this).data('target');
+		$('html, body').animate({ scrollTop : $(id).offset().top }, 'slow');
+		return false;
+	});
+});
