179a9767140b7d2912c0c3b953656ba1fe3954ac
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2015-07-28 20:59:48 +0200

    Optimize home js and css

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,17 +2,17 @@
 ---
 {% include js/jquery.js %}
 {% include js/bootstrap/transition.js %}
-{% include js/bootstrap/alert.js %}
-{% include js/bootstrap/button.js %}
-{% include js/bootstrap/carousel.js %}
+{% comment %}{% include js/bootstrap/alert.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/button.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/carousel.js %}{% endcomment %}
 {% include js/bootstrap/collapse.js %}
-{% include js/bootstrap/dropdown.js %}
-{% include js/bootstrap/modal.js %}
-{% include js/bootstrap/tooltip.js %}
-{% include js/bootstrap/popover.js %}
+{% comment %}{% include js/bootstrap/dropdown.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/modal.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/tooltip.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/popover.js %}{% endcomment %}
 {% include js/bootstrap/scrollspy.js %}
-{% include js/bootstrap/tab.js %}
-{% include js/bootstrap/affix.js %}
+{% comment %}{% include js/bootstrap/tab.js %}{% endcomment %}
+{% comment %}{% include js/bootstrap/affix.js %}{% endcomment %}
 {% include js/isotope.pkgd.js %}
 $(document).ready(function() {
 	/* Scrollspy */
